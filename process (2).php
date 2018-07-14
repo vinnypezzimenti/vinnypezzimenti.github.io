9a364fb9f78640c33bf86fe5b0ea421c5e9e3db2
vinnypezzimenti <41218187+vinnypezzimenti@users.noreply.github.com>
2018-07-14 14:47:53 -0400

    Add files via upload

---
+++ process (2).php
@@ -0,0 +1,27 @@
+<?php ob_start();
+
+
+
+	if(isset($_POST['submit'])) {
+		
+	$to = "vinnypezzimenti@gmail.com";
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$txt = $_POST['message'];
+	$headers = "From: " .$email . "\r\n" .
+	"CC: vinnyp45@yahoo.com";
+	
+	mail($to,$name,$subject,$txt,$headers);
+	
+	header("Location: first-website/contact.html");
+	
+
+		
+	
+	}
+
+
+
+
+?>
